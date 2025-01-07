554de1d60a7e99e519c4c4cb8a5a24f1cf09be6a
Okan Ay <okanay@Okan-MacBook-Pro-2.local>
2025-01-07 23:05:29 +0300

    upgrade hero 5

--- src/components/(main)/index/hero/suggestion-buttons.tsx
+++ src/components/(main)/index/hero/suggestion-buttons.tsx
@@ -4,41 +4,33 @@ import { useHorizontalWheelScroll } from "@/hooks/use-horizontal-wheel-scroll";
 import { useReviewStore } from "@/hooks/use-review-search";
 import { useEffect, useRef } from "react";
 
+const buttonLabels = [
+  "SPRAY SWEATER",
+  "BAGGY JEANS",
+  "WIDE LEG JEANS",
+  "SPRAY SWEATER",
+  "FIT JEANS",
+];
+
 export const SuggestionsButtons = () => {
   // prettier-ignore
   const { actions: { setSearch }} = useReviewStore();
 
   const ref = useHorizontalWheelScroll();
-  const containerRef = useRef(null);
-
-  const buttonLabels = [
-    "SPRAY SWEATER",
-    "BAGGY JEANS",
-    "WIDE LEG JEANS",
-    "SPRAY SWEATER",
-    "FIT JEANS",
-  ];
+  const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const scrollToCenter = () => {
-      if (ref.current) {
-        const { scrollWidth, clientWidth } = ref.current;
-        ref.current.scrollLeft = (scrollWidth - clientWidth) / 2;
-      }
-    };
-    if (containerRef.current) {
-      const container = containerRef.current as HTMLDivElement;
-      container.style.visibility = "hidden";
-      scrollToCenter();
-      container.style.visibility = "visible";
-    }
+    if (!ref.current || !containerRef.current) return;
+
+    const { scrollWidth, clientWidth } = ref.current;
+    ref.current.scrollLeft = (scrollWidth - clientWidth) / 2;
   }, [ref]);
 
   return (
-    <div className="relative flex flex-col gap-4">
-      <div className="pointer-events-none absolute left-0 z-10 flex h-10 w-full items-center justify-between sm:hidden">
-        <div className="h-full w-4 bg-gradient-to-r from-white to-transparent" />
-        <div className="h-full w-4 bg-gradient-to-l from-white to-transparent" />
+    <div className="relative flex-col gap-4">
+      <div className="pointer-events-none absolute left-0 z-10 flex h-11 w-full items-center justify-between sm:hidden">
+        <div className="h-full w-6 bg-gradient-to-r from-white/90 to-white/10" />
+        <div className="h-full w-6 bg-gradient-to-l from-white/90 to-white/10" />
       </div>
 
       <div
@@ -50,7 +42,7 @@ export const SuggestionsButtons = () => {
           <button
             key={index}
             onClick={() => setSearch(label)}
-            className="w-fit flex-shrink-0 rounded-sm border border-neutral-200 bg-white p-3 text-xs transition-[color_transform] duration-500 hover:border-primary-500 focus:outline-none focus:ring-0 active:scale-95"
+            className="h-11 w-fit flex-shrink-0 rounded-sm border border-neutral-200 bg-white px-3 text-xs transition-[color_transform] duration-500 hover:border-primary-500 focus:outline-none focus:ring-0 active:scale-95"
           >
             {label}
           </button>
